cb5c3164f7848b0f8e598dd2f0e24359fd56e443
Nate Dillon <natedillon@gmail.com>
2019-05-10 10:07:36 -0500

    Fixed link to natedillon.com

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 
 	<head>
-	
+
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 		<title>Nate and Jermaine</title>
@@ -11,17 +11,17 @@
 	</head>
 
 	<body>
-	
+
 		<h1>Nate and Jermaine</h1>
-	
+
 		<ul>
 			<li><a href="http://wedding.nateandjermaine.com/">Wedding</a></li>
-			<li><a href="http://www.urbanhaat.com/">Urban Haat</a></li>
-			<li><a href="http://www.natedillon.com/">Nate Dillon</a></li>
+			<li><a href="http://urbanhaat.com/">Urban Haat</a></li>
+			<li><a href="https://natedillon.com/">Nate Dillon</a></li>
 		</ul>
-		
+
 		<?php include('includes/analytics.php'); ?>
-	
+
 	</body>
 
 </html>
